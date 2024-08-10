61d08537f946e1cfc06d9fc8206ae1a484317104
David Fernando Guerrero Vanegas <dafer_guerrero@hotmail.com>
2024-08-10 16:40:25 -0500

    Creacion funcion mostrar datos dinamicamente HTML

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,9 @@
 import { Aprendiz, NivelEducativo } from "./aprendiz.js";
-export var ap = new Aprendiz("Juan Pablo", "Reyes Gómez", "avatar.png", 30, NivelEducativo.POSGRADO);
-console.log(ap);
+export var ap = new Aprendiz("David Fernando", "Guerrero vanegas", "myAvatar.png", 38, NivelEducativo.POSGRADO);
+var aprendizTable = document.getElementById("aprendiz");
+mostrarDatosAprendiz(ap);
+function mostrarDatosAprendiz(aprendiz) {
+    var tbodyAprendiz = document.createElement("tbody");
+    tbodyAprendiz.innerHTML = "<tr><td colspan=2><img src=\"./".concat(aprendiz.avatar, "\" height=\"100\"/td></tr>\n                            <tr><td>Nombres:<td>").concat(aprendiz.nombres, "</td>\n                            <tr><td>Apellidos:<td>").concat(aprendiz.apellidos, "</td></tr>\n                            <tr><td>Nivel educativo:<td>").concat(aprendiz.nivelEducativo, "</td></tr>\n                            <tr><td>Edad:<td>").concat(aprendiz.edad, "</td></tr>");
+    aprendizTable.appendChild(tbodyAprendiz);
+}
