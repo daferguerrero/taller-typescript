b4f5ae62b3a42523f767b1e9373ad5975fbfac14
David Fernando Guerrero Vanegas <dafer_guerrero@hotmail.com>
2024-08-12 22:18:07 -0500

    Integración función map a main.ts

--- main.ts
+++ main.ts
@@ -55,15 +55,18 @@ function mostrarEstadisticasAprendiz(aprendiz: Aprendiz): void {
 
 function mostrarCursosAprendiz(cursos: Curso[]): void {
   let tbodyCursos: HTMLElement = document.createElement("tbody");
+  let estado: string[] = cursos.map(c => (c.calificacion>60)?'green':'red');
+  let index: number = 0;
   for (let curso of cursos)
   {
     let trElement: HTMLElement = document.createElement("tr");
     trElement.innerHTML = `<td>${curso.nombre}</td></tr>
     <td>${curso.horas}</td>
-    <td>${curso.calificacion}</td>
+    <td style="color:${estado[index]}">${curso.calificacion}</td>
     <td>${curso.certificado}</td>
     <td>${curso.anio}</td>`
     tbodyCursos.appendChild(trElement)
+    index++;
   }
   cursosTable.appendChild(tbodyCursos);
 }
